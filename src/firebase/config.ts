3cf85933a7005efb11eeb94facc7f9b1c3433a7e
sammylla <kmc2442@kamiyama.ac.jp>
2026-03-02 17:30:49 +0900

    firebaseフォルダ追加

---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTzcmiK_wgix5oVZi1wyej_ucfwEVFwho",
+  authDomain: "womens-hackathon-92eef.firebaseapp.com",
+  projectId: "womens-hackathon-92eef",
+  storageBucket: "womens-hackathon-92eef.firebasestorage.app",
+  messagingSenderId: "522904297920",
+  appId: "1:522904297920:web:a01ab7b27c67186e13e825"
+};
+
+export const app = initializeApp(firebaseConfig);
