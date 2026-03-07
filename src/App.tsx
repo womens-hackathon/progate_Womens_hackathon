62c6389a2f260b3a13f75a37da6b089cec126074
sammylla <kmc2442@kamiyama.ac.jp>
2026-03-07 13:59:23 +0900

    App.tsx修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import VotePage from "./pages/VotePage";
 import GamePage from "./pages/TapGame";
-import CardGamePage from "./pages/cardGame/CardGamePage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<VotePage />} />
-      <Route path="/tap" element={<GamePage />} />
-      <Route
-        path="/card"
-        element={<CardGamePage appId="yourAppId" matchId="yourMatchId" />}
-      />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<VotePage />} />
+        <Route path="/tap" element={<GamePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
